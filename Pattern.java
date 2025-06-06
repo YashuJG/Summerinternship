13b7519a5e5b345a019bb7e1d7bb6b36108d474c
YashuJG <uwmabtw@gmail.com>
2025-06-06 16:07:56 +0530

    Initial commit

--- Pattern.java
+++ Pattern.java
@@ -6,7 +6,7 @@ class Pattern
  Scanner sc=new Scanner(System.in);
  System.out.println("enter no of rows");
  int r=sc.nextInt();
- for(int i=1;i<=r;i++)
+ for(int i=r;i>=1;i--)
  {
   for(int j=1;j<=i;j++)
  {
